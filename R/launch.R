182e40f6bc8f33a3e354b1a816bc1df22d3e4a0a
rjzupkoii <rjzii@yahoo.com>
2023-02-24 00:26:19 -0500

    lintr compliance

--- R/launch.R
+++ R/launch.R
@@ -1,9 +1,9 @@
 # launch.R
-# 
+#
 # Main entry point for the R Shiny app.
 library(shiny)
 
-source('ui.R')
-source('server.R')
+source("ui.R")
+source("server.R")
 
 shinyApp(ui = ui, server = server)
